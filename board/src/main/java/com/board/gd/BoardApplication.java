0f67d0822cb61415d439d079f752da767438fb79
gd.godong9 <gd.godong9@kakaocorp.com>
2017-04-06 10:42:58 +0900

    Add ApplicationConfiguration

--- board/src/main/java/com/board/gd/BoardApplication.java
+++ board/src/main/java/com/board/gd/BoardApplication.java
@@ -1,10 +1,8 @@
 package com.board.gd;
 
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -16,8 +14,4 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 }
