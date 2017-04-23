013a6873437e847cd41af0e55fc69c0c5353a5cb
GoDonghyun <godong9@gmail.com>
2017-04-23 12:48:24 +0900

    Update Signup

---
+++ client/src/components/Signup.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div class="signup">
+    <h1>{{ msg }}</h1>
+    <h2>Signup Page</h2>
+    <div>
+      <input type="text" v-model="name" placeholder="Name">
+      <input type="text" v-model="email" placeholder="Email">
+      <input type="password" v-model="password" placeholder="Password">
+      <button v-on:click="clickSignup">Signup</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'signup',
+    data() {
+      return {
+        msg: 'Signup page Message',
+        name: '',
+        email: '',
+        password: '',
+      };
+    },
+    methods: {
+      clickSignup: function clickSignup() {
+        this.$http.post('http://localhost:9000/users/signup', { name: this.name, email: this.email, password: this.password }).then((response) => {
+          // get body data
+          this.bodyData = response.body;
+          console.log(this.bodyData);
+        }, (response) => {
+          this.errorData = response.body;
+          // error callback
+        });
+      },
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+    font-size: 50px;
+  }
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Main from '@/components/Main';
+import Signup from '@/components/Signup';
 import Login from '@/components/Login';
 import Board from '@/components/Board';
 import Post from '@/components/Post';
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: Main,
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup,
     },
     {
       path: '/login',
