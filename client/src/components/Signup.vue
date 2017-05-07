28749c9a017b3ce46dc85051fe5e581458c1056a
paranpi <paranpi666@gmail.com>
2017-05-07 18:29:56 +0900

    [client] Singup Publishing

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,14 +1,12 @@
 
 <template>
-  <div class="signup">
-    <h1>{{ msg }}</h1>
-    <h2>Signup Page</h2>
-    <div>
-      <input type="text" v-model="name" placeholder="Name">
-      <input type="text" v-model="email" placeholder="Email">
-      <input type="password" v-model="password" placeholder="Password">
-      <button v-on:click="clickSignup">Signup</button>
-    </div>
+  <div class="sign-up">
+    <div class="title">상장사 직원들의 익명 주식 커뮤니티</div>
+    <div class="service-name">스탁 블라인드</div>
+    <div class="logo-wrapper"><img class="logo" src="../assets/logo.png"></div>
+    <div class="description">코스피, 코스닥에 상장된 회사에 다니고 있는 개인 투자자만 가입할 수 있는 비밀 주식 커뮤니티입니다. (서비스 이용료: 10,000/월)</div>
+    <div class="login-assistant">기존 사용자세요? <a src="">로그인</a></div>
+    <div class="button-sign-up"><button>회사 이메일로 가입하기</button></div>
   </div>
 </template>
 
@@ -40,9 +38,28 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-    font-size: 50px;
+  .sign-up {
+    position: relative;
+    margin: 10px 0 0 0;
+    height: 100%;
   }
+  .title {
+    font-weight: bold;
+    font-size: 10pt;
+  }
+  .service-name {
+    font-size : 20pt;
+    color : red;
+    font-weight: bold;
+  }
+  .logo {
+    max-width : 100px;
+    margin : 10px 0 10px 0;
+  }
+  .login-assistant {
 
+  }
+  .button-sign-up {
+
+  }
 </style>
