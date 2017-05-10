9341f0403bd2b4fd14d0e0a49220e46a0ba39617
paranpi <paranpi666@gmail.com>
2017-05-11 02:16:03 +0900

    [client] 반응형 스타일 적용

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -4,8 +4,8 @@
     <div class="title">상장사 직원들의 익명 주식 커뮤니티</div>
     <div class="service-name">스탁 블라인드</div>
     <div class="logo-wrapper"><img class="logo" src="../assets/logo.png"></div>
-    <div class="description">코스피, 코스닥에 상장된 회사에 다니고 있는 개인 투자자만 가입할 수 있는 비밀 주식 커뮤니티입니다. (서비스 이용료: 10,000/월)</div>
-    <div class="login-assistant">기존 사용자세요? <a src="">로그인</a></div>
+    <div class="description">코스피, 코스닥에 상장된 회사(계열사 포함)에 다니고 있는 개인 투자자만 가입할 수 있는 비밀 주식 커뮤니티입니다.<br>(서비스 이용료: 10,000/월) </div>
+    <div class="login-assistant">기존 사용자세요? <a href="/">로그인</a></div>
     <div class="button-sign-up"><button>회사 이메일로 가입하기</button></div>
   </div>
 </template>
@@ -39,27 +39,45 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .sign-up {
-    position: relative;
-    margin: 10px 0 0 0;
-    height: 100%;
+    padding: 5% 5% 0 5%;
   }
   .title {
     font-weight: bold;
-    font-size: 10pt;
+    font-size: 2.5vh;
   }
   .service-name {
-    font-size : 20pt;
+    font-size : 6vh;
     color : red;
     font-weight: bold;
+    margin: 2% 0 0 0;
+  }
+  .logo-wrapper {
+    margin: 25px 0 25px 0
   }
   .logo {
-    max-width : 100px;
-    margin : 10px 0 10px 0;
+    max-width : 30%;
+  }
+  .description {
+    word-break: keep-all;
+    word-wrap: break-word;
+    font-size: 2.2vh;
   }
   .login-assistant {
-
+    position: absolute;
+    width: 90%;
+    bottom: 15%;
   }
   .button-sign-up {
-
+    position: absolute;
+    width:90%;
+    bottom: 10px;
+  }
+  .button-sign-up button{
+    font-size: 3vh;
+    line-height: 8vh;
+    width: 100%;
+    color:white;
+    background-color: red;
+    max-width: 500px;
   }
 </style>
