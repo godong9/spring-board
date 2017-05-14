72efcf772c1f9a1cfe27699628e14095acedad93
GoDonghyun <godong9@gmail.com>
2017-05-14 11:51:14 +0900

    Add Company domain

---
+++ board/src/main/java/com/board/gd/domain/company/Company.java
@@ -0,0 +1,39 @@
+package com.board.gd.domain.company;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * Created by godong9 on 2017. 5. 14..
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "group_name")
+    private String groupName;
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "group_mail")
+    private String groupMail;
+
+    @Column(name = "company_mail")
+    private String companyMail;
+
+    @Column(name = "homepage")
+    private String homepage;
+}
