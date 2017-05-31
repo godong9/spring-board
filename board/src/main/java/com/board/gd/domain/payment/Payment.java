d226be063d907ec2ee0503335e5b5a34739025e9
GoDonghyun <godong9@gmail.com>
2017-05-31 22:41:23 +0900

    [Server] 결제 관련 도메인 업데이트, DB 업데이트

---
+++ board/src/main/java/com/board/gd/domain/payment/Payment.java
@@ -0,0 +1,39 @@
+package com.board.gd.domain.payment;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by godong9 on 2017. 5. 31..
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+}
---
+++ board/src/main/java/com/board/gd/domain/payment/PaymentResult.java
@@ -0,0 +1,7 @@
+package com.board.gd.domain.payment;
+
+/**
+ * Created by godong9 on 2017. 5. 31..
+ */
+public class PaymentResult {
+}
