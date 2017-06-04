2c43c18880bc16803dd1ebf6fbdcb63be0dc992c
GoDonghyun <godong9@gmail.com>
2017-06-04 16:38:22 +0900

    [Server] Doing 재결제 API

--- board/src/main/java/com/board/gd/domain/payment/PaymentController.java
+++ board/src/main/java/com/board/gd/domain/payment/PaymentController.java
@@ -7,6 +7,7 @@ import com.board.gd.iamport.SubscribeRequestDto;
 import com.board.gd.response.ServerResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,20 @@ public class PaymentController {
     @Autowired
     IamportManager iamportManager;
 
+    /**
+     * @api {post} /payments/subscribe Request Post payment subscribe
+     * @apiName PostSubscribePayment
+     * @apiGroup Payment
+     *
+     * @apiParam {String} card_number 카드 번호 (ex) 1111-2222-3333-4444)
+     * @apiParam {String} expiry 카드 유효기간 (ex) 2020-05)
+     * @apiParam {String} birth 생년월일 (ex) 900105)
+     * @apiParam {Number} pwd_2digit 패스워드 앞 두자리 (ex) 11)
+     *
+     * @apiSuccess {Number} status 상태코드
+     *
+     * @apiUse BadRequestError
+     */
     @PostMapping("/payments/subscribe")
     public ServerResponse postPaymentSubscribe(@RequestBody @Valid SubscribeRequestDto subscribeRequestDto) {
         User user = userService.getCurrentUser();
@@ -41,6 +56,23 @@ public class PaymentController {
         PaymentInfoDto paymentInfoDto = iamportManager.postSubscribeCustomer(subscribeRequestDto);
         paymentInfoDto.setUserId(user.getId());
         paymentService.createPaymentInfo(paymentInfoDto);
+        return ServerResponse.success();
+    }
+
+    /**
+     * @api {get} /payments/charge Request Get payment charge
+     * @apiName GetChargePayment
+     * @apiDescription 매일 12시에 결제 만료 예정인 유저들 재결제 요청
+     * @apiGroup Payment
+     *
+     * @apiSuccess {Number} status 상태코드
+     *
+     * @apiUse BadRequestError
+     */
+    @GetMapping("/payments/charge")
+    public ServerResponse getPaymentCharge() {
+        // TODO: UserRole에서 PAID이고 만료일이 24시간보다 적게 남은 것들 가져와서
+        // TODO: PaymentInfo에 enabled true인지 확인 후 Payment 생성해서 결제 요청
         return ServerResponse.success();
     }
 }
--- board/src/main/java/com/board/gd/domain/payment/PaymentRequestDto.java
+++ board/src/main/java/com/board/gd/domain/payment/PaymentRequestDto.java
@@ -8,4 +8,5 @@ import lombok.Data;
 
 @Data
 public class PaymentRequestDto {
+    private Long userId;
 }
