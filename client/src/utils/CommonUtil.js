63af0559c9ade8918d89ecef26e967e09274800d
paranpi <paranpi666@gmail.com>
2017-07-11 23:46:20 +0900

    [web] api module 추가

--- client/src/utils/CommonUtil.js
+++ client/src/utils/CommonUtil.js
@@ -2,25 +2,21 @@ import moment from 'moment';
 
 function ServerPlugin() {}
 
-/*eslint-disable */
-ServerPlugin.install = function (Vue) {
-  Vue.prototype.getServerPath = function (path) {
-    return process.env.SERVER_HOST + path;
-  };
+ServerPlugin.install = (Vue) => {
+  Vue.prototype.getServerPath = path => process.env.SERVER_HOST + path;
 
-  Vue.prototype.errorHandler = function (response) {
+  Vue.prototype.errorHandler = (response) => {
     if (response.body && response.body.error) {
       alert(response.body.error.message);
     }
   };
-
-  Vue.prototype.validateEmail = function (email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  Vue.prototype.validateEmail = (email) => {
+    // eslint-disable-next-line no-useless-escape
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   };
 
   Vue.prototype.moment = moment;
 };
-/*eslint-enable */
 
 export default ServerPlugin;
