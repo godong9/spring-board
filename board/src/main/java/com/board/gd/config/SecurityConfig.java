22539ae11ac128f537f06e682f86fcf6a1b58f72
GoDonghyun <godong9@gmail.com>
2017-07-15 19:07:49 +0900

    [Server] CORS OPTIONS 설정 수정

--- board/src/main/java/com/board/gd/config/SecurityConfig.java
+++ board/src/main/java/com/board/gd/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.board.gd.config;
 import com.board.gd.auth.EmailAuthenticationProvider;
 import com.board.gd.domain.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").access("permitAll")
                 .antMatchers("/companies/**").access("permitAll")
                 .antMatchers("/stocks/**").access("permitAll")
                 .antMatchers("/users/login", "/users/signup", "/users/email", "/users/*/auth", "/users/data").access("permitAll")
